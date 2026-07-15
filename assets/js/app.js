3f4f9dcf0e8b6003fa73b9f7cd0faf0f9fa28373
thoompranay-maker <thoompranay@gmail.com>
2026-07-15 17:14:23 +0530

    Update app.js

--- assets/js/app.js
+++ assets/js/app.js
@@ -26,6 +26,44 @@ if (topBtn) {
 
 }
 
+/*==============================
+    Scroll Reveal Start
+==============================*/
+
+const reveals = document.querySelectorAll(
+'.reveal,.reveal-left,.reveal-right'
+);
+
+const observer = new IntersectionObserver(
+
+(entries)=>{
+
+entries.forEach(entry=>{
+
+if(entry.isIntersecting){
+
+entry.target.classList.add("active");
+
+}
+
+});
+
+},
+
+{
+
+threshold:0.15
+
+}
+
+);
+
+reveals.forEach(el=>observer.observe(el));
+
+/*==============================
+    Scroll Reveal End
+==============================*/
+
 // --- AOS  --- //
 
 /* ------- 
